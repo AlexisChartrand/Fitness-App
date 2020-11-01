cfb2702188e52be2125074fe93d80d6f9703f6ae
Alex Chartrand <44973499+AlexChartrand440@users.noreply.github.com>
2020-11-01 15:20:25 -0500

    Initial Push
    Initial Push

---
+++ FitProgramUI-master/app/src/main/java/com/example/example/fitprogram/StartWorkActTwo.java
@@ -0,0 +1,125 @@
+package com.example.example.fitprogram;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class StartWorkActTwo extends AppCompatActivity {
+
+    TextView intropage, subintropage, fitonetitle, fitonedesc, timerValue, btnexercise;
+    View divpage, bgprogress;
+    ImageView imgtimer;
+    LinearLayout fitone;
+
+    private static final long START_TIME_IN_MILLIS = 2400000;
+    private CountDownTimer countDownTimer;
+    private boolean mTimerRunning;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+
+    Animation btthree, bttfour, ttbone, ttbtwo, alphagogo;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_work_two);
+
+        // load animations
+        btthree = AnimationUtils.loadAnimation(this, R.anim.btthree);
+        bttfour = AnimationUtils.loadAnimation(this, R.anim.bttfour);
+        ttbone = AnimationUtils.loadAnimation(this, R.anim.ttbone);
+        ttbtwo = AnimationUtils.loadAnimation(this, R.anim.ttbtwo);
+        alphagogo = AnimationUtils.loadAnimation(this, R.anim.alphagogo);
+
+
+        // import font
+        Typeface MLight = Typeface.createFromAsset(getAssets(), "fonts/MLight.ttf");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
+        Typeface Vidaloka = Typeface.createFromAsset(getAssets(), "fonts/Vidaloka.ttf");
+
+
+        intropage = (TextView) findViewById(R.id.intropage);
+        subintropage = (TextView) findViewById(R.id.subintropage);
+        btnexercise = (TextView) findViewById(R.id.btnexercise);
+        fitonetitle = (TextView) findViewById(R.id.fitonetitle);
+        fitonedesc = (TextView) findViewById(R.id.fitonedesc);
+        timerValue = (TextView) findViewById(R.id.timerValue);
+        btnexercise = (TextView) findViewById(R.id.btnexercise);
+
+        divpage = (View) findViewById(R.id.divpage);
+        bgprogress = (View) findViewById(R.id.bgprogress);
+        fitone = (LinearLayout) findViewById(R.id.fitone);
+        imgtimer = (ImageView) findViewById(R.id.imgtimer);
+
+        // change customize font
+        intropage.setTypeface(Vidaloka);
+        subintropage.setTypeface(MLight);
+        btnexercise.setTypeface(MMedium);
+        timerValue.setTypeface(MMedium);
+        fitonedesc.setTypeface(MLight);
+        fitonetitle.setTypeface(MMedium);
+
+        // assign animation
+        btnexercise.startAnimation(bttfour);
+        bgprogress.startAnimation(btthree);
+        fitone.startAnimation(ttbone);
+        intropage.startAnimation(ttbtwo);
+        subintropage.startAnimation(ttbtwo);
+        divpage.startAnimation(ttbtwo);
+        timerValue.startAnimation(alphagogo);
+        imgtimer.startAnimation(alphagogo);
+
+        startTimer();
+
+        // give an event to another page
+        btnexercise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a = new Intent(StartWorkActTwo.this, StartWorkAcThree.class);
+                a.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(a);
+            }
+        });
+
+    }
+
+    private void startTimer(){
+        countDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDowntText();
+            }
+
+            @Override
+            public void onFinish() {
+                AlertDialog alertDialog = new AlertDialog.Builder(StartWorkActTwo.this).create();
+                alertDialog.setTitle("Cardio Exercise Completed");
+                alertDialog.setIcon(R.drawable.cardio);
+                alertDialog.setMessage("Congratulations you have done it! You have completed the cardio exercise.");
+                alertDialog.show();
+            }
+        }.start();
+        mTimerRunning = true;
+    }
+
+    private void updateCountDowntText(){
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeft = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        timerValue.setText(timeLeft);
+    }
+
+}
